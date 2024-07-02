223f66dc4e0f32934765993e9ed1b0c61ac200e3
camilolp21 <camilo.loaiza@onegroupit.com>
2024-07-02 10:21:53 -0500

    [commit][glue]: Feat: URI added as a secret

--- src/glue_jobs/etl_mongo_collections.py
+++ src/glue_jobs/etl_mongo_collections.py
@@ -1,11 +1,26 @@
 import sys
+import boto3
+import json
 from awsglue.transforms import *
 from awsglue.utils import getResolvedOptions
 from pyspark.context import SparkContext
 from awsglue.context import GlueContext
 from awsglue.job import Job
 
-args = getResolvedOptions(sys.argv, ['JOB_NAME'])
+def get_secret(secret_name):
+    region_name = "us-east-2"
+
+    client = boto3.client('secretsmanager', region_name=region_name)
+
+    get_secret_value_response = client.get_secret_value(SecretId=secret_name)
+    secret = get_secret_value_response['SecretString']
+    return json.loads(secret)
+
+args = getResolvedOptions(sys.argv, ['JOB_NAME', 'secret-name'])
+secret_name = args['secret-name']
+secret = get_secret(secret_name)
+mongo_uri_base = secret['mongo_uri']
+
 s3_output_path = "654654288333-dl-bronze"
 
 sc = SparkContext()
@@ -16,7 +31,6 @@ job.init(args['JOB_NAME'], args)
 
 database = "myspace1a_rc"
 collections = ["users", "cities", "offices", "rooms"]
-mongo_uri_base = "mongodb://root:oneG-TrYh4Ck%2aTh15@3.210.8.173:27017/?authSource=admin&readPreference=primary&directConnection=true&ssl=false"
 
 for collection in collections:
     mongo_uri = f"{mongo_uri_base}&database={database}&collection={collection}"

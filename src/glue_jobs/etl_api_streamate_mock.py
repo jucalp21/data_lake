1ab9cfcec0ae73e7bab96064f8b08e48abf36da6
camilolp21 <camilo.loaiza@onegroupit.com>
2024-07-17 17:15:09 -0500

    [commit][main] ETL updated with mock api requirements

--- src/glue_jobs/etl_api_streamate_mock.py
+++ src/glue_jobs/etl_api_streamate_mock.py
@@ -18,6 +18,7 @@ job.init(args["JOB_NAME"], args)
 
 s3_output_path = "s3://data-lake-demo/bronze/"
 api_url = "https://devstreamatemock.omgworldwidegroup.com/api/v1/studio/earningsdailysummary"
+users_api_url = "https://devstreamatemock.omgworldwidegroup.com/api/v1/user"
 token = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJlbWFpbEFkZHJlc3MiOiJ0ZXN0c3R1ZGlvMTIzNEBzdHJlYW1hdGVtb2RlbHMuY29tIiwiaWF0IjoxNzIxMjMyODE3LCJleHAiOjE3NTI3OTA0MTd9.wXwM86yl9w-PiQVCVNW-9ljGCudPBki77YSC7esmuwI"
 
 try:
@@ -25,6 +26,7 @@ try:
         "token": token
     }
 
+    # Consultar datos de estudios y performadores
     response = requests.post(api_url, json=payload)
     response.raise_for_status()
     data = response.json()
@@ -55,8 +57,33 @@ try:
 
             for earning in performer['earnings']:
                 earnings_by_performer_df.append((earning['date'], int(earning['onlineSeconds']), float(
-                    earning['payableAmount']), performer["performerId"]))
+                    earning['payableAmount']), performer["emailAddress"]))
 
+    # Consultar datos de modelos
+    response = requests.get(users_api_url)
+    response.raise_for_status()
+    users_data = response.json()
+
+    if not users_data.get('success', False):
+        raise ValueError("La consulta de usuarios no fue exitosa")
+
+    users = users_data['users']
+
+    models_df = []
+
+    for user in users:
+        models_df.append((
+            user["artisticName"],
+            user["jasminUser"],
+            user["office"],
+            user["room"],
+            user["streamateUser"],
+            user["_id"],
+            user["city"],
+            user["user"]
+        ))
+
+    # Crear DataFrames de Spark a partir de las listas
     df_studios = spark.createDataFrame(
         studios_df, ["studioId", "emailAddress"])
     df_earnings_by_studio = spark.createDataFrame(
@@ -64,19 +91,39 @@ try:
     df_performers = spark.createDataFrame(
         performers_df, ["performerId", "nickname", "emailAddress"])
     df_earnings_by_performer = spark.createDataFrame(earnings_by_performer_df, [
-                                                     "date", "onlineSeconds", "payableAmount", "performerId"])
+                                                     "date", "onlineSeconds", "payableAmount", "emailAddress"])
+    df_models = spark.createDataFrame(models_df, [
+        "artisticName", "jasminUser", "office", "room", "streamateUser", "_id", "city", "user"])
 
+    # Unir performers con models usando el streamateUser
+    df_performers_with_id = df_performers.join(
+        df_models, df_performers.emailAddress == df_models.streamateUser, "left").select(
+            df_performers["*"], df_models["_id"].alias("model_id"))
+
+    # Unir earnings_by_performers con models usando el streamateUser y el emailAddress
+    df_earnings_by_performer_with_id = df_earnings_by_performer.join(
+        df_models, df_earnings_by_performer.emailAddress == df_models.streamateUser, "left").select(
+            df_earnings_by_performer["*"], df_models["_id"].alias("model_id"))
+
+    # Almacenar studios en S3
     df_studios.write.mode("overwrite").json(
         f"{s3_output_path}/streamatemock/studios/data_{datetime.now().strftime('%Y-%m-%d_%H-%M-%S')}.json")
 
+    # Almacenar earnings_by_studio en S3
     df_earnings_by_studio.write.mode("overwrite").json(
         f"{s3_output_path}/streamatemock/earnings_by_studio/data_{datetime.now().strftime('%Y-%m-%d_%H-%M-%S')}.json")
 
-    df_performers.write.mode("overwrite").json(
+    # Almacenar performers en S3
+    df_performers_with_id.write.mode("overwrite").json(
         f"{s3_output_path}/streamatemock/performers/data_{datetime.now().strftime('%Y-%m-%d_%H-%M-%S')}.json")
 
-    df_earnings_by_performer.write.mode("overwrite").json(
+    # Almacenar earnings_by_performer en S3
+    df_earnings_by_performer_with_id.write.mode("overwrite").json(
         f"{s3_output_path}/streamatemock/earnings_by_performer/data_{datetime.now().strftime('%Y-%m-%d_%H-%M-%S')}.json")
+
+    # Almacenar models en S3
+    df_models.write.mode("overwrite").json(
+        f"{s3_output_path}/streamatemock/models/data_{datetime.now().strftime('%Y-%m-%d_%H-%M-%S')}.json")
 
 except Exception as e:
     raise e
